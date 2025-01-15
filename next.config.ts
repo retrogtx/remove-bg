ef5574e00ebee8c87896adb5ee5b9b5696f136f1
Amrit <iamamrit27@gmail.com>
2025-01-15 15:28:34 +0530

    feat: add experimental server actions with increased body size limit in Next.js config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ['@prisma/client'],
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '100mb'
+    }
+  }
 }
 
 module.exports = nextConfig 
